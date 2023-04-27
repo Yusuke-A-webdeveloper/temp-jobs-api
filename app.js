d0ba8fe3156142a88e09f516d371a336545d0105
Yusuke A <jpenwebdesign@gmail.com>
2023-04-27 13:39:40 -0700

    nodemon to node

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// security package
 app.set('trust proxy', 1);
 app.use(
   rateLimiter({
