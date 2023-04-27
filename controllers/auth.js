1523f60e39eb1fbffed31a89ed0e4e8d4d8b4b70
Yusuke A <jpenwebdesign@gmail.com>
2023-04-27 13:32:47 -0700

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
+
+const register = async (req, res) => {
+  ////// mongoose will take care of it //////////
+  //   const { name, email, password } = req.body;
+  //   if (!name || !email || !password) {
+  //     throw new BadRequestError('Please provide name, email and password');
+  //   }
+  ////// mongoose will take care of it //////////
+
+  ////// hashing password in middleware User.js //////////
+  // const { name, email, password } = req.body;
+  // const salt = await bcrypt.genSalt(10);
+  // const hashedPassword = await bcrypt.hash(password, salt);
+  // const tempUser = { name, email, password: hashedPassword };
+  // const user = await User.create({ ...tempUser });
+  ////// hashing password in middleware User.js //////////
+
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  // compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
+
+module.exports = {
+  login,
+  register,
+};
---
+++ models/Job.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['inteerview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      // chain with user id that is created in User model
+      // so only the user can modify their posts
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Job', JobSchema);
